4f78f6f833726a77d365a8189ab6a2128e06e6b0
Enrico <e.pezzotti006@studenti.unibs.it>
2024-06-21 15:02:45 +0200

    Rimuovi cache di Git

--- Backup/test_read.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import copy
-
-retrieved_segments = pickle.load(open('segments.dump', 'rb'))
-
-print(type(retrieved_segments))
-print(type(retrieved_segments[0]))
-print(type((retrieved_segments[0][0])))
-print(type((retrieved_segments[0][0][0])))
